02bb59a4c4cec9221c631356c75c917079262f03
mihai <mihaipopa00@gmail.com>
2026-06-14 12:29:22 +0300

    Added local five server config

---
+++ fiveserver.config.js
@@ -0,0 +1,6 @@
+// https://github.com/yandeu/five-server#watch--ignore
+// Only watch the changes from the frontend repo to not get automatically refreshed when logs
+// folder changes are done
+// Either of the two work:
+export const watch = 'frontend';
+// export const ignore = 'backend/logs';
