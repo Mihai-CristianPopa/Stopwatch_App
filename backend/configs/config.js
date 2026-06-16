a9017012577039234ec12e1abf1d76132fd26d90
mihai <mihaipopa00@gmail.com>
2026-06-16 17:48:07 +0300

    backend: group mongo credential into a single env variable

--- backend/configs/config.js
+++ backend/configs/config.js
@@ -2,9 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  mongoUser: process.env.MONGO_USER,
-  mongoPassword: process.env.MONGO_PASSWORD,
-  mongoClusterHost: process.env.MONGO_CLUSTER_HOST,
+  mongoUri: process.env.MONGO_URI,
   port: process.env.PORT || 7000,
   frontendOrigin: process.env.FRONTEND_ORIGIN || "http://localhost:5173",
 };
--- backend/db/mongoClient.js
+++ backend/db/mongoClient.js
@@ -1,12 +1,7 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import { config } from "../configs/config.js";
 
-const mongoUser = config.mongoUser;
-const mongoPassword = config.mongoPassword;
-
-const uri = `mongodb+srv://${mongoUser}:${mongoPassword}@map-navigation.lim17rk.mongodb.net/?retryWrites=true&w=majority&appName=Map-Navigation`;
-
-const client = new MongoClient(uri, {
+const client = new MongoClient(config.mongoUri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
